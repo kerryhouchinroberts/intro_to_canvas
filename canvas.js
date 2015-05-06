ee97251c8781142bf549551b7ab8a40d3358d60b
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-05-06 10:40:30 -0600

    Add rectangles to canvas

--- canvas.js
+++ canvas.js
@@ -1,6 +1,27 @@
-// function drawBorder(){
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getContext('2d');
-//     var size = { x: canvas.width, y: canvas.height }
-//     context.strokeRect(0,0, size.x, size.y);
-// }
+function drawRectangle(){
+   var canvas = document.getElementById("canvas");
+   var context = canvas.getContext('2d');
+   var size = { x: canvas.width, y: canvas.height };
+    context.strokeStyle = "Purple";
+    context.strokeRect(0,0, size.x, size.y);
+    context.fillStyle = "Orange";
+    context.fillRect(10, 10, (size.x - 20), (size.y - 20));
+
+}
+
+function quarterRectangle(){
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = { x: canvas.width, y: canvas.height };
+  context.fillStyle = "Blue";
+  context.fillRect(10, 10, (size.x/2), (size.y/2));
+}
+
+function redRectangle(){
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext('2d');
+  var size = { x: canvas.width, y: canvas.height };
+  context.fillStyle = "Red";
+  context.fillRect(((size.x/2)-10), ((size.y/2)-10), (size.x/2), (size.y/2));
+}
+drawRectangle();
